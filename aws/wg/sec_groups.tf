1772a82772f355670db4c6fbe2da22e063aea6d9
David Fry <david.fry@modusbox.com>
2021-09-17 11:08:02 +0100

    fix security_groups ref

--- aws/wg/sec_groups.tf
+++ aws/wg/sec_groups.tf
@@ -43,21 +43,21 @@ resource "aws_security_group" "wireguard_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = "51820"
     to_port     = "51820"
     protocol    = "udp"
-    cidr_blocks = [aws_security_group.alb_sg.id]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   ingress {
     from_port   = "5000"
     to_port     = "5000"
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.alb_sg.id]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   tags = merge({}, var.tags)
