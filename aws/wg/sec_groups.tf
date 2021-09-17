1e24d683e051bf89194806e99372bedb4e1bb16a
David Fry <david.fry@modusbox.com>
2021-09-17 12:12:12 +0100

    nlb don't support sgs

--- aws/wg/sec_groups.tf
+++ aws/wg/sec_groups.tf
@@ -1,32 +1,6 @@
-resource "aws_security_group" "alb_sg" {
-  vpc_id      = var.vpc_id
-  name        = "${var.wireguard_tenancy_name}-wireguard-alb"
-  description = "Allow all to vpn alb"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = "-1"
-    to_port     = "-1"
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge({}, var.tags)
+data "aws_subnet" "selected" {
+  id = var.subnet_id
 }
-
 resource "aws_security_group" "wireguard_sg" {
   vpc_id      = var.vpc_id
   name        = "${var.wireguard_tenancy_name}-wireguard"
@@ -49,20 +23,20 @@ resource "aws_security_group" "wireguard_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks = [data.aws_subnet.selected.cidr_block]
   }
   ingress {
     from_port   = "51820"
     to_port     = "51820"
     protocol    = "udp"
-    security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks = [data.aws_subnet.selected.cidr_block]
   }
 
   ingress {
     from_port   = "5000"
     to_port     = "5000"
     protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks = [data.aws_subnet.selected.cidr_block]
   }
 
   tags = merge({}, var.tags)
