391d80c2388672bf52178d71470ea8be1f94d92c
Oscar R Cobar <oscar.cobar@modusbox.com>
2021-11-15 00:55:47 +0100

    accesing data set

--- aws/tgw-vpn-s2s/pm4ml-routes-side/main.tf
+++ aws/tgw-vpn-s2s/pm4ml-routes-side/main.tf
@@ -12,7 +12,7 @@ variable "destination_cidr_block" {
 
 
 resource "aws_route" "this" {
-  for_each               = var.route_table_ids
+  for_each               = var.route_table_ids[each.key]
   route_table_id         = tolist(each.value)
   destination_cidr_block = var.destination_cidr_block
   transit_gateway_id     = var.transit_gateway_id
