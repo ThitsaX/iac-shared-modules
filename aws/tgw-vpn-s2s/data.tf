f72619d5c48703799a52ac9248f67bef6db1bee4
Oscar R Cobar <oscar.cobar@modusbox.com>
2021-11-15 00:44:47 +0100

    removing filter wildcard

--- aws/tgw-vpn-s2s/data.tf
+++ aws/tgw-vpn-s2s/data.tf
@@ -2,7 +2,7 @@ data "aws_route_tables" "all" {
   for_each = toset(var.pm4ml_routes_table_names)
   filter {
     name   = "tag:Name"
-    values = [each.value]["*"]
+    values = [each.value]
   }
 
 }
