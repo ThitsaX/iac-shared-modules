301ff3e994cf4126d21eea2db493a0cead05c4fd
Oscar R Cobar <oscar.cobar@modusbox.com>
2021-11-15 08:09:49 +0100

    replace for each by count

--- aws/tgw-vpn-s2s/pm4ml-routes-side/main.tf
+++ aws/tgw-vpn-s2s/pm4ml-routes-side/main.tf
@@ -10,11 +10,18 @@ variable "destination_cidr_block" {
 
 }
 
-
 resource "aws_route" "this" {
+  count                  = length(var.route_table_ids)
+  route_table_id         = tolist(var.route_table_ids)[count.index]
+  destination_cidr_block = var.destination_cidr_block
+  transit_gateway_id     = var.transit_gateway_id
+}
+
+/* resource "aws_route" "this" {
   for_each               = var.route_table_ids
   route_table_id         = tostring(each.value.ids)
   destination_cidr_block = var.destination_cidr_block
   transit_gateway_id     = var.transit_gateway_id
 
 }
+ */
